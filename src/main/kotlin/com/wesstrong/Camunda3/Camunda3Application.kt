21712aed044444c7b886cb19de6b78146af786d5
snoop244 <snewport@wesstrong.com>
2017-06-24 14:09:51 -0400

    Initial commit

---
+++ src/main/kotlin/com/wesstrong/Camunda3/Camunda3Application.kt
@@ -0,0 +1,11 @@
+package com.wesstrong.Camunda3
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Camunda3Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Camunda3Application::class.java, *args)
+}
